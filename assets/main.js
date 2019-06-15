4e5ab3a215335a9d352ed727bf2653a9870b9d1d
francois gleyze <francois.gleyze@lrqdo.fr>
2019-06-15 15:27:53 +0200

    rewording : wall > member

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,12 @@ const leftStudValue = document.getElementById('js-select-leftStud');
 const rightStudValue = document.getElementById('js-select-rightStud');
 
 const changeWidth = (width) => {
-    document.getElementById('wallTemplate').style.width = width * 2 + "px";
+    document.getElementById('wall').style.width = width * 2 + "px";
     document.getElementById('js-display-wallWidth').innerHTML = width + " cms";
 }
 
 const changeHeight = (height) => {
-    document.getElementById('wallTemplate').style.height = height * 2 + "px";
+    document.getElementById('wall').style.height = height * 2 + "px";
     document.getElementById('js-display-wallHeight').innerHTML = height + " cms";
 }
 
@@ -26,20 +26,20 @@ wallHeightValue.onkeyup = () => {
 
 leftStudValue.onchange = () => {
     if (leftStudValue.value === 'female') {
-        document.getElementById('js-bottomPlate').classList.add("wall--bottomPlate--leftFemale");
-        document.getElementById('js-leftStud').classList.add("wall--leftStud--female");
+        document.getElementById('js-bottomPlate').classList.add("member--bottomPlate--leftFemale");
+        document.getElementById('js-leftStud').classList.add("member--leftStud--female");
     } else {
-        document.getElementById('js-bottomPlate').classList.remove("wall--bottomPlate--leftFemale");
-        document.getElementById('js-leftStud').classList.remove("wall--leftStud--female");
+        document.getElementById('js-bottomPlate').classList.remove("member--bottomPlate--leftFemale");
+        document.getElementById('js-leftStud').classList.remove("member--leftStud--female");
     }
 }
 
 rightStudValue.onchange = () => {
     if (rightStudValue.value === 'female') {
-        document.getElementById('js-bottomPlate').classList.add("wall--bottomPlate--rightFemale");
-        document.getElementById('js-rightStud').classList.add("wall--rightStud--female");
+        document.getElementById('js-bottomPlate').classList.add("member--bottomPlate--rightFemale");
+        document.getElementById('js-rightStud').classList.add("member--rightStud--female");
     } else {
-        document.getElementById('js-bottomPlate').classList.remove("wall--bottomPlate--rightFemale");
-        document.getElementById('js-rightStud').classList.remove("wall--rightStud--female");
+        document.getElementById('js-bottomPlate').classList.remove("member--bottomPlate--rightFemale");
+        document.getElementById('js-rightStud').classList.remove("member--rightStud--female");
     }
 }
