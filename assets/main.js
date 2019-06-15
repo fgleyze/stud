ac0f0ac8655964a8b2494126c713f74a4bf64b53
francois gleyze <francois.gleyze@lrqdo.fr>
2019-06-15 15:09:49 +0200

    sides of wall can be set as female

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,7 @@
 const frameWidthValue = document.getElementById('js-input-frameWidth');
 const frameHeightValue = document.getElementById('js-input-frameHeight');
+const leftStudValue = document.getElementById('js-select-leftStud');
+const rightStudValue = document.getElementById('js-select-rightStud');
 
 const changeWidth = (width) => {
     document.getElementById('frameTemplate').style.width = width * 2 + "px";
@@ -19,7 +21,25 @@ frameWidthValue.onkeyup = () => {
 }
 
 frameHeightValue.onkeyup = () => {
-    changeHeight(frameHeightValue.value);
-    
+    changeHeight(frameHeightValue.value);  
 }
 
+leftStudValue.onchange = () => {
+    if (leftStudValue.value === 'female') {
+        document.getElementById('js-bottomPlate').classList.add("frame--bottomPlate--leftFemale");
+        document.getElementById('js-leftStud').classList.add("frame--leftStud--female");
+    } else {
+        document.getElementById('js-bottomPlate').classList.remove("frame--bottomPlate--leftFemale");
+        document.getElementById('js-leftStud').classList.remove("frame--leftStud--female");
+    }
+}
+
+rightStudValue.onchange = () => {
+    if (rightStudValue.value === 'female') {
+        document.getElementById('js-bottomPlate').classList.add("frame--bottomPlate--rightFemale");
+        document.getElementById('js-rightStud').classList.add("frame--rightStud--female");
+    } else {
+        document.getElementById('js-bottomPlate').classList.remove("frame--bottomPlate--rightFemale");
+        document.getElementById('js-rightStud').classList.remove("frame--rightStud--female");
+    }
+}
