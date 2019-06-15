76de71a66351b95ffe0a833e73c2533ce330505e
francois gleyze <francois.gleyze@lrqdo.fr>
2019-06-15 12:00:09 +0200

    Refactor js by removing duplicated innerHTML updates

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,24 @@
 const frameWidthValue = document.getElementById('js-input-frameWidth');
 const frameHeightValue = document.getElementById('js-input-frameHeight');
 
-document.getElementById('js-display-frameWidth').innerHTML = frameWidthValue.value;
-document.getElementById('js-display-frameHeight').innerHTML = frameHeightValue.value;
-
 const changeWidth = (width) => {
     document.getElementById('frameTemplate').style.width = width * 2 + "px";
+    document.getElementById('js-display-frameWidth').innerHTML = width + " cms";
 }
 
 const changeHeight = (height) => {
     document.getElementById('frameTemplate').style.height = height * 2 + "px";
+    document.getElementById('js-display-frameHeight').innerHTML = height + " cms";
 }
 
 changeWidth(400);
 changeHeight(240);
 
-document.getElementById('frameTemplate').style.height = 240 * 2 + "px";
-
 frameWidthValue.onkeyup = () => {
-    document.getElementById('js-display-frameWidth').innerHTML = frameWidthValue.value + " cms";
     changeWidth(frameWidthValue.value);
 }
 
 frameHeightValue.onkeyup = () => {
-    document.getElementById('js-display-frameHeight').innerHTML = frameHeightValue.value + " cms";
     changeHeight(frameHeightValue.value);
     
 }
