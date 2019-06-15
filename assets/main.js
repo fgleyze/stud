c0e091c429debe9154211ef0d323ec3a308f7657
francois gleyze <francois.gleyze@lrqdo.fr>
2019-06-15 15:16:10 +0200

    rewording : frame > wall

--- assets/main.js
+++ assets/main.js
@@ -1,45 +1,45 @@
-const frameWidthValue = document.getElementById('js-input-frameWidth');
-const frameHeightValue = document.getElementById('js-input-frameHeight');
+const wallWidthValue = document.getElementById('js-input-wallWidth');
+const wallHeightValue = document.getElementById('js-input-wallHeight');
 const leftStudValue = document.getElementById('js-select-leftStud');
 const rightStudValue = document.getElementById('js-select-rightStud');
 
 const changeWidth = (width) => {
-    document.getElementById('frameTemplate').style.width = width * 2 + "px";
-    document.getElementById('js-display-frameWidth').innerHTML = width + " cms";
+    document.getElementById('wallTemplate').style.width = width * 2 + "px";
+    document.getElementById('js-display-wallWidth').innerHTML = width + " cms";
 }
 
 const changeHeight = (height) => {
-    document.getElementById('frameTemplate').style.height = height * 2 + "px";
-    document.getElementById('js-display-frameHeight').innerHTML = height + " cms";
+    document.getElementById('wallTemplate').style.height = height * 2 + "px";
+    document.getElementById('js-display-wallHeight').innerHTML = height + " cms";
 }
 
 changeWidth(400);
 changeHeight(240);
 
-frameWidthValue.onkeyup = () => {
-    changeWidth(frameWidthValue.value);
+wallWidthValue.onkeyup = () => {
+    changeWidth(wallWidthValue.value);
 }
 
-frameHeightValue.onkeyup = () => {
-    changeHeight(frameHeightValue.value);  
+wallHeightValue.onkeyup = () => {
+    changeHeight(wallHeightValue.value);  
 }
 
 leftStudValue.onchange = () => {
     if (leftStudValue.value === 'female') {
-        document.getElementById('js-bottomPlate').classList.add("frame--bottomPlate--leftFemale");
-        document.getElementById('js-leftStud').classList.add("frame--leftStud--female");
+        document.getElementById('js-bottomPlate').classList.add("wall--bottomPlate--leftFemale");
+        document.getElementById('js-leftStud').classList.add("wall--leftStud--female");
     } else {
-        document.getElementById('js-bottomPlate').classList.remove("frame--bottomPlate--leftFemale");
-        document.getElementById('js-leftStud').classList.remove("frame--leftStud--female");
+        document.getElementById('js-bottomPlate').classList.remove("wall--bottomPlate--leftFemale");
+        document.getElementById('js-leftStud').classList.remove("wall--leftStud--female");
     }
 }
 
 rightStudValue.onchange = () => {
     if (rightStudValue.value === 'female') {
-        document.getElementById('js-bottomPlate').classList.add("frame--bottomPlate--rightFemale");
-        document.getElementById('js-rightStud').classList.add("frame--rightStud--female");
+        document.getElementById('js-bottomPlate').classList.add("wall--bottomPlate--rightFemale");
+        document.getElementById('js-rightStud').classList.add("wall--rightStud--female");
     } else {
-        document.getElementById('js-bottomPlate').classList.remove("frame--bottomPlate--rightFemale");
-        document.getElementById('js-rightStud').classList.remove("frame--rightStud--female");
+        document.getElementById('js-bottomPlate').classList.remove("wall--bottomPlate--rightFemale");
+        document.getElementById('js-rightStud').classList.remove("wall--rightStud--female");
     }
 }
